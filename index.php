0f5c658ec6638e9e5ade5f6be3aed6563a0d168f
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-11 22:47:35 -0400

    added 3 forms

--- index.php
+++ index.php
@@ -1,15 +1,73 @@
 <?php
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
-
+require_once('models/data-layer.php');
+session_start();
 $f3 = Base::instance();
 
 $f3->route('GET / ', function(){
    $view = new Template();
    echo $view->render('views/home.html');
+});
+
+$f3->route('GET|POST /start ', function($f3){
+    $genders = getGender();
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION['genders'] = $_POST['genders'];
+        $_SESSION['first'] = $_POST['first'];
+        $_SESSION['last'] = $_POST['last'];
+        $_SESSION['phoneNumber'] = $_POST['phoneNumber'];
+        $f3->reroute('start2');
+    }
+
+    $f3->set('genders', $genders);
+
+    $view = new Template();
+    echo $view->render('views/start.html');
+});
+
+$f3->route('GET|POST /start2 ', function ($f3){
+
+    $states = getState();
+    $genders = getGender();
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION['genderPage2'] = $_POST['gender'];
+        $_SESSION['state'] = $_POST['state'];
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['bio'] = $_POST['bio'];
+        $f3->reroute('start3');
+        session_destroy();
+    }
+
+    $f3->set('genders', $genders);
+    $f3->set('states', $states);
+    $view = new Template();
+    echo $view->render('views/start2.html');
+});
+
+$f3->route('GET|POST /start3 ', function($f3){
+    $outdoors = getOutDoorInterests();
+    $indoors = getInDoorInterests();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION['indoor'] = $_POST['indoor'];
+        $_SESSION['outdoor'] = $_POST['outdoor'];
+        $f3->reroute('summary');
+        session_destroy();
+    }
+
+    $f3->set('indoors', $indoors);
+    $f3->set('outdoors', $outdoors);
+
+    $view = new Template();
+    echo $view->render('views/start3.html');
+});
+
+$f3->route('GET /summary ', function($f3){
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 $f3-> run();
---
+++ models/data-layer.php
@@ -0,0 +1,64 @@
+<?php
+
+function getGender(){
+    return array("Male", "Female");
+}
+
+function getInDoorInterests(){
+    return array("tv", "movies", "cooking", "board games", "puzzles", "reading", "playing cards", "video games");
+}
+
+function getOutDoorInterests(){
+    return array("hiking", "biking", "swimming", "collecting", "walking", "climbing");
+}
+
+function getState(){
+    return array("Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "IllinoisIndiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "MontanaNebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming");
+}
